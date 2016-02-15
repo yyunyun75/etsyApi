fd1f46585b5d7c6a8b3cb73a298b10f63deb8785
Leslie Lee <leslie.lee@hollywood.com>
2016-02-15 16:08:04 -0500

    search function

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ test etsy api and search
  *  Install node, bower
  *  Run ``bower install`` to install bootstrap and angular in the lib folder
  *  Run ``node app.js`` to view the index.html file
+
+*  Notes
+ *   search results is bookmarkable
+ *   Only tested in chrome and firefox browser in windows
--- app/app.js
+++ app/app.js
@@ -3,18 +3,33 @@
 
 	var app = angular.module('apiApp', []);
 
+	app.config(['$locationProvider', function($locationProvider){
+		$locationProvider.html5Mode(true);
+	}]);
 
-	app.controller('appCtrl', ['$scope', '$http',
-		function($scope, $http){
-			$http.jsonp('https://openapi.etsy.com/v2/listings/active.js?fields=listing_id,title,price,url,tags&includes=Images:1:0&api_key=umb36tq9mcay4qh71njauw7b&callback=JSON_CALLBACK').then(
+	app.controller('appCtrl', ['$scope', '$http', '$location',
+		function($scope, $http, $location){
+			$http.jsonp('https://openapi.etsy.com/v2/listings/active.js?fields=listing_id,title,price,url,description,tags&includes=Images:1:0&api_key=umb36tq9mcay4qh71njauw7b&callback=JSON_CALLBACK').then(
 				function(results){
-					$scope.items = results.data.results;
-					console.log($scope.items);
+					var tags = $location.search().tags;
+					if(!!tags){
+						$scope.items = [];
+						angular.forEach(results.data.results, function(value, key) {
+						 	if(value.title.indexOf(tags)>-1 || value.tags.indexOf(tags) > -1 || value.description.indexOf(tags) > -1){
+						 		this.push(value);
+						 	}
+						}, $scope.items);
+					}else{
+						$scope.items = results.data.results;
+					}			
+
 				},
 				function(error){
 					console.log('error getting data', error);
 				}
 			);
+
+
 
 		}
 	]);
