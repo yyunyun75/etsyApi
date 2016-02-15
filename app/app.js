1065ebde6eb825e30f14cd788167ba73bd801424
Leslie Lee <leslie.lee@hollywood.com>
2016-02-15 15:24:14 -0500

    search and listing

--- app/app.js
+++ app/app.js
@@ -6,9 +6,10 @@
 
 	app.controller('appCtrl', ['$scope', '$http',
 		function($scope, $http){
-			$http.jsonp('https://openapi.etsy.com/v2/listings/active.js?api_key=umb36tq9mcay4qh71njauw7b&callback=JSON_CALLBACK').then(
+			$http.jsonp('https://openapi.etsy.com/v2/listings/active.js?fields=listing_id,title,price,url,tags&includes=Images:1:0&api_key=umb36tq9mcay4qh71njauw7b&callback=JSON_CALLBACK').then(
 				function(results){
-					console.log(results.data.results);
+					$scope.items = results.data.results;
+					console.log($scope.items);
 				},
 				function(error){
 					console.log('error getting data', error);
